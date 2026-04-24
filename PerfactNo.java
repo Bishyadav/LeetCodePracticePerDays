40e4466700397e152fba28c72a75f9bd6ce05caf
Bishyadav <bisheshyadav422@gmail.com>
2026-04-24 23:22:14 +0530

    IsomorphicStringOrNot

--- PerfactNo.java
+++ PerfactNo.java
@@ -1,20 +1,39 @@
 package org.example;
 
+import java.util.HashMap;
+
 public class PerfactNo {
     public static void main(String[] args) {
-        int no=7;
-        boolean b=chechNoIsPerfactOrNot(no);
+        String s="abba";
+        String tr="cat dog dog cat ";
+        String[] t=tr.split("\\s");
+        boolean b=isIsomorphic(s,t);
         System.out.println(b);
 
     }
-    public static boolean chechNoIsPerfactOrNot(int no){
-        int sum=0;
-        for(int i=1;i<no;i++){
-            if(no%i==0){
-                sum=sum+i;
+        public static boolean isIsomorphic(String s, String[] t) {
+            HashMap<Character, Integer> charIndexS = new HashMap<>();
+            HashMap<String, Integer> charIndexT = new HashMap<>();
+            
+         if(s.length()!=t.length) return false;
+            for (int i = 0; i < s.length(); i++) {
+                if (!charIndexS.containsKey(s.charAt(i))) {
+                    charIndexS.put(s.charAt(i), i);
+                }
+
+                if (!charIndexT.containsKey(t[i])) {
+                    charIndexT.put(t[i], i);
+                }
+
+                if (!charIndexS.get(s.charAt(i)).equals(charIndexT.get(t[i]))) {
+                    return false;
+                }
+                // for c# replace like for get() with
+              //  if (d1[s[i]] != d2[t[i]]){
+               //     Console.WriteLine("false");
+               // }
             }
+
+            return true;
         }
-        if(sum==no) return true;
-        else return false;
     }
-}
